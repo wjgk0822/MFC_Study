731a26ba6dd591793adc850072b00dbed9e23801
정환 김 <wjgk0822@naver.com>
2023-10-18 00:49:30 +0900

    MFC_Study2 commit

---
+++ MFC_Study2/MFC_Study2/MFC_Study2Dlg.h
@@ -0,0 +1,39 @@
+﻿
+// MFC_Study2Dlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CMFCStudy2Dlg 대화 상자
+class CMFCStudy2Dlg : public CDialogEx
+{
+
+private:
+	CImage m_image;
+// 생성입니다.
+public:
+	CMFCStudy2Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFC_STUDY2_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedBtnImage();
+};
